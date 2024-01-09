e159647d4d142c410894aaf10c1e11e2208848d7
Carlos Schults <carlos.schults@gmail.com>
2024-01-09 08:53:47 -0300

    Edit to negative rule

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const readline = require('readline').createInterface({
   readline.question('Enter a list of numbers separated by comma:\n', numbers => {
     let integers = numbers.split(',').map(x => parseInt(x) || 0);
     let negatives = integers.filter(x => x < 0);
-    if (negatives.length > 0) {
+    if (negatives.length >= 0) {
       throw new Error(`Negatives not allowed: ${negatives.join(', ')}.`);
     }
     
