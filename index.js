241526646f3c298786f1654ed96c480a17951f98
Carlos Schults <carlos.schults@gmail.com>
2024-01-11 08:37:04 -0300

    Minor formattings

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  readline.question('Enter a list of numbers separated by comma:\n', numbers => {
-    let integers = numbers.split(',').map(x => parseInt(x) || 0);
-    let negatives = integers.filter(x => x < 0);
-    if (negatives.length >= 0) {
-      throw new Error(`Negatives not allowed: ${negatives.join(', ')}.`);
-    }
-    
-    let sum = integers
-      .filter(x => x <= 1000)
-      .reduce((a, b) => a + b, 0);
-    console.log(`The sum of the entered numbers is ${sum}.`);
-    readline.close();
-  });
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Enter a list of numbers separated by comma:\n', numbers => {
+  let integers = numbers.split(',').map(x => parseInt(x) || 0);
+  let negatives = integers.filter(x => x < 0);
+
+  if (negatives.length >= 0) {
+    throw new Error(`Negatives not allowed: ${negatives.join(', ')}.`);
+  }
+
+  let sum = integers
+    .filter(x => x <= 1000)
+    .reduce((a, b) => a + b, 0);
+
+  console.log(`The sum of the entered numbers is ${sum}.`);
+  readline.close();
+});
